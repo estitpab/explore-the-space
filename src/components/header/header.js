1c6dadfffeaf7207f88208b6dc76a0b0e14dd274
Baptiste <baptiste@enviedefraise.fr>
2020-06-09 00:52:17 +0200

    Add header

---
+++ src/components/header/header.js
@@ -0,0 +1,21 @@
+import { Link } from "gatsby"
+import React from "react"
+import styles from "./header.module.scss"
+
+const Header = () => (
+  <header className={styles.header}>
+    <div className="logo">
+      <Link to="/">Voyager</Link>
+    </div>
+    <nav>
+      <Link to="/news">News</Link>
+      <Link to="/observing">Observing</Link>
+      <Link to="/resources-education">Resources & Education</Link>
+      <Link to="/community">Community</Link>
+      <Link to="/about-us">About us</Link>
+    </nav>
+
+  </header>
+)
+
+export default Header
